929790c69e4a91da70ef374947edcf6e8cf1af10
tavferreira <tiagoferreira@tuta.io>
2020-03-09 19:27:04 +0100

    Loans

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
 
-export const Main = () => {
-    const [ssn, setSsn] = useState('')
+export const Main = (props) => {
     const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
+
+    const [ssn, setSsn] = useState('')
+    const [test, setTest] = useState(false)
+
+
     const ssnData = ssn !== '' ? "&input_username=" + ssn : ""
+    const testData = test ? '&test=true' : ''
     const link =
         "https://link.tink.com/1.0/authorize/?" +
         "client_id=" +
@@ -11,7 +16,8 @@ export const Main = () => {
         "&redirect_uri=http://localhost:3000/callback" +
         "&scope=accounts:read" +
         ssnData +
-        "&market=SE&locale=en_US"
+        "&market=SE&locale=en_US" +
+        testData
 
     return (
         <div>
@@ -19,8 +25,12 @@ export const Main = () => {
                 SSN
                 <input type='text' name="ssn" value={ssn} onChange={e => setSsn(e.target.value)} />
             </label>
+            <label>
+                Test providers
+                <input type='checkbox' name='test' value={test} onChange={e => setTest(!test)} checked={test} />
+            </label>
             <a href={link}><button type="button">
                 Go to bank</button></a>
-        </div>
+        </div >
     )
 }
